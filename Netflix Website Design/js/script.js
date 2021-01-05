53c87ccd1b776f4d1934123899ca1e39e3c4fe3a
Yilmaz Kadan <kadan8080@gmail.com>
2021-01-05 23:34:15 +0300

    Responsive and Netflix Finished Design

--- Netflix Website Design/js/script.js
+++ Netflix Website Design/js/script.js
@@ -1,17 +1,12 @@
 window.onload = function(){
-
-
-
 	//ACCORDİON ITEMS
 	var acc = document.getElementsByClassName("accordion-button");
 	var acc_array = Array.from(acc);
 	acc_array.forEach(function(acc){
 		acc.addEventListener("click", function() {
-			var icon = acc.getElementsByTagName('i');
-			var icon_array = Array.from(icon);
-			icon_array.forEach(function(icons){
-				icons.classList.toggle("display-none");
-			});
+			var svg = acc.getElementsByTagName('svg');
+				svg[0].classList.toggle("svg-closed");
+				svg[0].classList.toggle("svg-open");
 			var answer = this.nextElementSibling;
 			answer.classList.toggle('active');	
 		});
